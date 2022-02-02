4892eaab55c099e5f31f4920d0507224095ea939
Jctech12 <75099518+Jctech12@users.noreply.github.com>
2022-02-02 13:00:11 -0800

    Added bootstraop

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/Jctech12/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Janet Choe
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Jctech12/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced in Guthub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Jctech12/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Janet Choe
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Jctech12/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced in Guthub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
