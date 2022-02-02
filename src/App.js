4052ebd4e01a7120a880e74b8e96e58c7ffb459a
Jctech12 <75099518+Jctech12@users.noreply.github.com>
2022-02-02 11:50:23 -0800

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import react from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="#" target="_blank">
+          Janet Choe
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Jctech12/weather-react" target="_blank">
+          open-sourced in Guthub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
