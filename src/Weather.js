9fe01947f5072c62a6a750e80674c6872af74f4d
Jctech12 <75099518+Jctech12@users.noreply.github.com>
2022-02-07 16:21:08 -0800

    Refactored Weather data information into WeatherInfo component.  Implemented search logic

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 import { FaSearch } from "react-icons/fa";
@@ -6,81 +7,72 @@ import { FaSearch } from "react-icons/fa";
 export default function Weather() {
   const [loading, setLoading] = useState(true);
   const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState("Irvine");
+
+  useEffect(() => {
+    async function fetchData() {
+      const apiKey = "15b01518d9470d65eb96b19937333ceb";
+      let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+      let weatherData = await axios.get(apiUrl);
+      handleResponse(weatherData);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
   function handleResponse(response) {
     console.log(response.data);
-    console.log(response.data.main.humidity);
     setWeatherData({
       temperature: Math.round(response.data.main.temp),
       wind: Math.round(response.data.wind.speed),
       city: response.data.name,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.dt * 1000),
     });
   }
 
-  useEffect(() => {
-    async function fetchData() {
-      const apiKey = "15b01518d9470d65eb96b19937333ceb";
-      let defaultCity = "Irvine";
-      let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${apiKey}&units=imperial`;
+  async function search() {
+    setLoading(true);
+    const apiKey = "15b01518d9470d65eb96b19937333ceb";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-      await axios.get(apiUrl).then(handleResponse);
-      setLoading(false);
-    }
-    fetchData();
-  }, []);
+    let weatherData = await axios.get(apiUrl);
+    handleResponse(weatherData);
+    setLoading(false);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
   if (loading) {
     return <div>Loading</div>;
   } else {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <section className="search">
               <input
                 type="search"
                 placeholder="Type a city.."
                 autoComplete="off"
+                onChange={handleCityChange}
               />
               <button className="search-icon btn-light">
                 <FaSearch />
               </button>
             </section>
           </div>
-        </form>
-        <header>
-          <h2>{weatherData.city}</h2>
-          <span>Last updated:</span>
-          <h2>{weatherData.date}</h2>
-        </header>
-        <main className="overview">
-          <img
-            src="https://openweathermap.org/img/wn/01n@2x.png"
-            alt="Clear Sky"
-          />
-          <h3>{weatherData.description}</h3>
-        </main>
-        <section>
-          <div className="row">
-            <div className="col-6">
-              <h1>
-                {weatherData.temperature}
-                <span className="unit">℉</span>
-              </h1>
-            </div>
-            <div className="col-6">
-              <ul className="detail">
-                <li>Humidity: {weatherData.humidity}%</li>
-                <li>
-                  Wind: {weatherData.wind}{" "}
-                  <span className="text-lowercase">mph</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
+        </form>{" "}
+        <WeatherInfo data={weatherData} />
       </div>
     );
   }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,42 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeathrInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <header>
+        <h2>{props.data.city}</h2>
+        <span>Last updated:</span>
+        <h2>
+          <FormattedDate date={props.data.date} />
+        </h2>
+      </header>
+      <main className="overview">
+        <img
+          src="https://openweathermap.org/img/wn/01n@2x.png"
+          alt="Clear Sky"
+        />
+        <h3>{props.data.description}</h3>
+      </main>
+      <section>
+        <div className="row">
+          <div className="col-6">
+            <h1>
+              {props.data.temperature}
+              <span className="unit">℉</span>
+            </h1>
+          </div>
+          <div className="col-6">
+            <ul className="detail">
+              <li>Humidity: {props.data.humidity} %</li>
+              <li>
+                Wind: {props.data.wind}
+                <span className="text-lowercase"> mph</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
